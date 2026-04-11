cfe4396320103628a7e035a5975bf3915e9b931d
Suresh Kumar <suresh.bandarulanka@gmail.com>
2026-04-11 10:57:29 +0530

    fix static export

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+
   serverExternalPackages: ["jspdf", "fflate"],
+
   images: {
+    unoptimized: true,   // 🔥 MUST ADD
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'godavari-specials.firebasestorage.app',
-      }
-    ],
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
+      { protocol: 'https', hostname: 'godavari-specials.firebasestorage.app' }
+    ]
   },
 };
 
