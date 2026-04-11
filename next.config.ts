d087533cccf80dc0204c34490a2b1aaaee44cfbf
Suresh Kumar <suresh.bandarulanka@gmail.com>
2026-04-11 11:54:16 +0530

    setup server deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-
   serverExternalPackages: ["jspdf", "fflate"],
 
   images: {
-    unoptimized: true,   // 🔥 MUST ADD
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
